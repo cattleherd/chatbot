17693a43c9792a8ff5ee6466733c99e3e1d76218
Radwan Ahmed <radwan.hussein@protonmail.com>
2023-04-21 17:46:12 -0400

    commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    // Enable WebAssembly support
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
 
-module.exports = nextConfig
+    // Configure the module type for WebAssembly files
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "webassembly/async",
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
