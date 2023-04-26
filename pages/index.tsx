ee8a4a799019dc55007497139d569a26689ca9ad
Radwan Ahmed <radwan.hussein@protonmail.com>
2023-04-26 18:42:37 -0400

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   // Define an async function to fetch data from the API route
   const fetchData = async () => {
     try {
-      const response = await fetch("/api/openai", {
+      const response = await fetch("/api/questions", {
         method: "POST", //sending data to api must be post
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question }), //must send as json object
@@ -40,10 +40,23 @@ export default function Home() {
       if (err instanceof Error) {
         alert(err.message);
       } else {
-        alert('An unexpected error occurred.');
+        alert("An unexpected error occurred.");
       }
     }
   };
+
+  const injestpdf = async()=>{
+    try{
+      const response = await fetch('/api/pdf-parser',{
+        method:"GET",
+        headers: { "Content-Type": "application/json" },
+      })
+      const result = await response.json()
+      console.log(result)
+    }catch(err){
+      console.log(err)
+    }
+  }
 
   // Render the fetched data if available
   const renderData = () => {
@@ -74,8 +87,11 @@ export default function Home() {
             mr={"20px"}
             placeholder="ask a question"
           ></Textarea>
-          <Button height={"100%"} onClick={fetchData}>
+          <Button height={"60%"} onClick={fetchData} width={'100px'} fontSize={'smaller'}>
             Query
+          </Button>
+          <Button height={"60%"} marginLeft={'20px'} width={'100px'} fontSize={'smaller'} onClick={injestpdf}>
+            Injest pdf
           </Button>
         </CardFooter>
       </Card>
